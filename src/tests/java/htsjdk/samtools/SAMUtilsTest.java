258fd0ea9355aec8a4b793c03ead7a612898c825
Nils Homer <nilshomer@gmail.com>
2015-11-09 21:58:19 -0500

    Adding useful utility methods to clip overlapping bases for mapped pairs.

--- src/tests/java/htsjdk/samtools/SAMUtilsTest.java
+++ src/tests/java/htsjdk/samtools/SAMUtilsTest.java
@@ -26,6 +26,8 @@ package htsjdk.samtools;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 public class SAMUtilsTest {
     @Test
     public void testCompareMapqs() {
@@ -41,5 +43,36 @@ public class SAMUtilsTest {
         Assert.assertTrue(SAMUtils.compareMapqs(1, 0) > 0);
         Assert.assertTrue(SAMUtils.compareMapqs(1, 255) > 0);
         Assert.assertTrue(SAMUtils.compareMapqs(2, 1) > 0);
+    }
+
+    @Test
+    public void testClippingOfRecordWithSoftClipBasesAtTheEnd() {
+        /**
+         * Tests that if we need to clip a read with soft-clipping at the end, it does the right thing.
+         */
+
+        // setup the record
+        final SAMFileHeader header = new SAMFileHeader();
+        header.addSequence(new SAMSequenceRecord("1", 1000));
+        final SAMRecord record = new SAMRecord(header);
+        record.setReadPairedFlag(true);
+        record.setCigar(new Cigar(Arrays.asList(new CigarElement(5, CigarOperator.M), new CigarElement(5, CigarOperator.S)))); // 5M5S
+        record.setReferenceIndex(0);
+        record.setAlignmentStart(1);
+        record.setMateAlignmentStart(5); // should overlap by one base now
+        record.setMateReferenceIndex(0);
+        record.setReadBases("AAAAAAAAAA".getBytes());
+
+        final int numToClip = SAMUtils.getNumOverlappingBasesToClip(record);
+        Assert.assertEquals(numToClip, 1);
+
+        SAMUtils.clipOverlappingBases(record, numToClip, false); // Side-effects are OK
+
+        final Cigar cigar = record.getCigar();
+        Assert.assertEquals(cigar.numCigarElements(), 2);
+        Assert.assertEquals(cigar.getCigarElement(0).getLength(), 4); // 4M
+        Assert.assertEquals(cigar.getCigarElement(0).getOperator(), CigarOperator.M); // 4M
+        Assert.assertEquals(cigar.getCigarElement(1).getLength(), 6); // 6S
+        Assert.assertEquals(cigar.getCigarElement(1).getOperator(), CigarOperator.S); // 6S
     }
 }
